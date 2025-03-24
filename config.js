535ae66e08c31a26bea8a37e688e94e7a3691234
XxInfernoGamingxX <168434709+MMYTHICFALCONBOT@users.noreply.github.com>
2025-03-24 15:05:33 +0530

    Update config.js

--- config.js
+++ config.js
@@ -11,6 +11,6 @@ module.exports = {
         clientId: "a568b55af1d940aca52ea8fe02f0d93b",
         clientSecret: "e8199f4024fe49c5b22ea9a3dd0c4789"
     },
-    botToken: "MTM1MjgwMjc3NTU5MTAzMDg4Nw.GMbcow.O999IxaYujpjap1bdIk9bngJ_i4YxcMDMGu7mE",
+    botToken: "MTM1MjgwMjc3NTU5MTAzMDg4Nw.GzK4b1.qZbwYEzBIGnEMJDFa_2WjHD-RFy2BEr1OZBMvE",
     embedColor: "#0061ff"
 };
